3e1345f397c4d3ae6df28c5b60ce8300e696b895
Krlostks <carlospixel13@gmail.com>
2024-02-14 13:50:36 -0600

    Adding ListTitle to the ListView

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -8,31 +8,46 @@ class HomeScreen extends StatelessWidget {
     return  Scaffold(
       appBar: AppBar(
         title: const Text("componentes de flutter"),
+        
       ) ,
         body:ListView(children:const [
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          Text('Hola'),
-          ],
+          ListTile(
+            title: Text('Holaaaaaaaaa', 
+                    style: 
+                    TextStyle(
+                      color: Color.fromARGB(255, 1, 143, 32)
+                    )),
+            subtitle: Text('Adios'),
+            leading: Icon(Icons.app_settings_alt, color: Colors.indigo,),
+            trailing: Icon(Icons.arrow_right),
+          ),
+          Divider(
+            color: Colors.pinkAccent,
+          ),
+          ListTile(
+            leading: Icon(Icons.add_location, color: Colors.indigo),
+            title: Text('Holaaaaaaaaa', 
+                    style: 
+                    TextStyle(
+                      color: Color.fromARGB(255, 1, 143, 32)
+                    )),
+            subtitle: Text('Adios'),
+            trailing: Icon(Icons.arrow_right),
+          ),
+          Divider(
+            color: Colors.red,
+          ),
+          ListTile(
+            leading: Icon(Icons.admin_panel_settings_rounded, color: Colors.indigo,),
+            title: Text('Holaaaaaaaaa', 
+                    style: 
+                    TextStyle(
+                      color: Color.fromARGB(255, 1, 143, 32)
+                    )),
+            subtitle: Text('Adios'),
+            trailing: Icon(Icons.arrow_right),
+          )
+        ],
         ),
       );
   }
