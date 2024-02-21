2aa19097f1d84057928b43e161071605e34b05d5
Krlostks <carlospixel13@gmail.com>
2024-02-21 13:32:56 -0600

    Navigator.push on all the screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,6 @@
+import 'package:componentes/screens/infinit_list_screen.dart';
+import 'package:componentes/screens/inputs_screen.dart';
+import 'package:componentes/screens/notifications_screen.dart';
 import 'package:componentes/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
@@ -14,11 +17,17 @@ class HomeScreen extends StatelessWidget {
       
         body:ListView(children: [
           ListTile(
-            title: Text('Holaaaaaaaaa',
+            title: Text('Entradas',
             style: Theme.of(context).textTheme.headlineLarge,),
             subtitle: Text('Diferentes widgets para entradas de flutter', style:  Theme.of(context).textTheme.bodySmall),
             leading: const Icon(Icons.app_settings_alt, color: AppTheme.iconos),
             trailing: const Icon(Icons.arrow_right),
+            onTap:() {
+              final ruta1 = MaterialPageRoute(builder: (context){
+                return const InputScreen();
+              });
+              Navigator.push(context, ruta1);
+            },
           ),
           const Divider(
             color: Color.fromARGB(255, 255, 255, 255),
@@ -29,6 +38,12 @@ class HomeScreen extends StatelessWidget {
             style: Theme.of(context).textTheme.headlineLarge,),
             subtitle: Text('Scroll infinito', style:  Theme.of(context).textTheme.bodySmall,),
             trailing: const Icon(Icons.list_alt_rounded),
+            onTap:() {
+              final ruta2 = MaterialPageRoute(builder: (context){
+                return const InfinitListScreen();
+              });
+              Navigator.push(context, ruta2);
+            },
           ),
           const Divider(
             color: Color.fromARGB(255, 255, 255, 255),
@@ -39,6 +54,12 @@ class HomeScreen extends StatelessWidget {
             style: Theme.of(context).textTheme.headlineLarge,),
             subtitle:  Text('Notificaciones', style:  Theme.of(context).textTheme.bodySmall),
             trailing: const Icon(Icons.notification_add),
+            onTap:() {
+              final ruta3 = MaterialPageRoute(builder: (context){
+                return const NotificationsScreen();
+              });
+              Navigator.push(context, ruta3);
+            },
           )
         ],
         ),
